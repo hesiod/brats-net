08ada5fad71d0877d994626288541ccd69019778
Tobias Markus <tobias@miglix.eu>
2020-07-02 22:17:41 +0200

    Simplify Context classes and add explicit parameters

--- train.py
+++ train.py
@@ -14,16 +14,6 @@ import model.utils
 import model.loss
 
 
-# %%
-
-# Identifier for this group of runs
-meta_name = 'brats4'
-batch_size = 32
-num_workers = 6
-num_epochs = 50
-lr = 0.0001
-should_check = False
-
 
 # %%
 
@@ -36,12 +26,9 @@ class Context:
             self.net.apply(model.unet.init_weights)
 
         self.device = model.utils.try_gpu()
-        self.run_iter = 0
-        if criterion:
-            self.criterion = criterion
-        else:
-            self.criterion = model.loss.Loss()
         print('Using device {}'.format(self.device))
+
+        self.run_iter = 0
 
     def check_topology(self):
         dummy_input = torch.randn(size=(2, 4, 240, 240), dtype=torch.float32)
@@ -53,29 +40,35 @@ class Context:
         torch.onnx.export(self.net, dummy_input, filename, verbose=True)
 
 class TrainContext:
-    def __init__(self, context, data_context):
+    def __init__(self, context, data_context, criterion, lr, batch_size, experiment_name='unnamed'):
         self.ctx = context
         self.data = data_context
         self.global_iter = 1
+        self.experiment_name = experiment_name
+
+        self.criterion = criterion
+        self.batch_size = batch_size
 
         self.optimizer = torch.optim.SGD(self.ctx.net.parameters(), lr=lr)
         #optimizer = torch.optim.RMSprop(net.parameters(), lr=lr, weight_decay=1e-8, momentum=0.9)
         #self.scheduler = ReduceLROnPlateau(self.optimizer, 'min')
 
-        self.brats_train_perepoch = self.data.split_data(num_epochs)
-
-    def run(self):
+    def run(self, num_epochs):
         self.ctx.run_iter += 1
-        run_name = 'runs/run_{}_{}_{}'.format(meta_name, lr, self.ctx.run_iter)
+        run_name = 'runs/run_{}_{}_{}'.format(self.experiment_name, lr, self.ctx.run_iter)
 
         print('Commencing {}'.format(run_name))
         self.writer = SummaryWriter(os.path.join('runs', run_name))
+
         print('Writing graph')
-        X = torch.randn(size=(2, 4, 240, 240), dtype=torch.float32)
-        self.writer.add_graph(self.ctx.net, X)
+        dummy_input = torch.randn(size=(2, 4, 240, 240), dtype=torch.float32)
+        self.writer.add_graph(self.ctx.net, dummy_input)
+        del dummy_input
         print('done')
 
         self.ctx.net.to(self.ctx.device)
+
+        self.brats_train_perepoch = self.data.split_data(num_epochs)
 
         for epoch in range(num_epochs-1):
             self.run_epoch(epoch)
@@ -83,7 +76,7 @@ class TrainContext:
         self.writer.close()
 
     def checkpoint(self):
-        torch.save(self.ctx.net.state_dict(), 'checkpoints/checkpoint_{}_{}.pt'.format(meta_name, self.ctx.run_iter))
+        torch.save(self.ctx.net.state_dict(), 'checkpoints/checkpoint_{}_{}.pt'.format(self.experiment_name, self.ctx.run_iter))
 
     def run_epoch(self, epoch):
         train_iter = td.DataLoader(self.brats_train_perepoch[epoch], batch_size, shuffle=True, num_workers=num_workers)
@@ -94,14 +87,14 @@ class TrainContext:
         
         print('Total batches: {}'.format(batch_count))
 
-        self.train_loss_epoch = 0.0
+        train_loss_epoch = 0.0
         for i, (X, y) in enumerate(train_iter):
             batch_loss = self.run_batch(i, X, y)
-            self.train_loss_epoch += batch_loss
+            train_loss_epoch += batch_loss
             print('batch {:4}/{} batchloss {}'.format(i+1, batch_count, batch_loss))
-        self.train_loss_epoch /= batch_count
+        train_loss_epoch /= batch_count
 
-        self.writer.add_scalar('loss/train', self.train_loss_epoch, self.global_iter)
+        self.writer.add_scalar('loss/train', train_loss_epoch, self.global_iter)
 
         self.ctx.net.eval()
         test_loss_epoch = 0.0
@@ -110,7 +103,7 @@ class TrainContext:
                 X_test = X_test.float().to(self.ctx.device)
                 y_test = y_test.float().to(self.ctx.device)
                 y_test_hat = self.ctx.net(X_test).squeeze(1)
-                b_l = self.ctx.criterion(y_test_hat, y_test)
+                b_l = self.criterion(y_test_hat, y_test)
                 test_loss_epoch += float(b_l)
             test_loss_epoch /= len(test_iter)
 
@@ -118,6 +111,7 @@ class TrainContext:
         self.writer.flush()
 
         self.checkpoint()
+
 
         print('epoch {}/{}, train loss {}, test loss {}'.format(epoch+1,
             num_epochs, self.train_loss_epoch, test_loss_epoch))
@@ -130,7 +124,7 @@ class TrainContext:
         y = y.float().to(self.ctx.device)
         y_hat = self.ctx.net(X).squeeze(1)
 
-        l = self.ctx.criterion(y_hat, y)
+        l = self.criterion(y_hat, y)
 
         self.optimizer.zero_grad()
         l.backward()
@@ -162,6 +156,17 @@ class TrainContext:
 
 dctx = model.brats_dataset.DataSplitter()
 
+
+# %%
+
+# Identifier for this group of runs
+meta_name = 'brats4'
+batch_size = 32
+num_workers = 6
+num_epochs = 50
+lr = 0.0001
+should_check = False
+
 # %%
 
 ctx = Context('checkpoint_sd.pt')
@@ -173,11 +178,13 @@ if should_check:
 
 # %%
 
-tctx = TrainContext(ctx, dctx)
+criterion = model.loss.Loss()
+
+tctx = TrainContext(ctx, dctx, criterion=criterion, lr=lr, batch_size=batch_size, experiment_name=meta_name)
 
 # %%
 
-tctx.run()
+tctx.run(num_epochs)
 
 # %%
 

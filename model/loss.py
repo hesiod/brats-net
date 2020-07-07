d2f5bfec43343ec7c47bf6dc9a26870faf6d351b
Tobias Markus <tobias@miglix.eu>
2020-07-07 16:54:20 +0200

    Fix Dice loss
    Add missing Sigmoid layer before dice loss.

--- model/loss.py
+++ model/loss.py
@@ -1,28 +1,36 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 __all__ = ['Loss']
 
 
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+        self.smooth = 1.
 
-# https://github.com/pytorch/pytorch/issues/1249
-def dice_loss(pred, target):
-    smooth = 1.
+    # https://github.com/pytorch/pytorch/issues/1249
+    def forward(self, pred, target):
+        iflat = pred.view(-1)
+        tflat = target.view(-1)
+        intersection = (iflat * tflat).sum()
 
-    iflat = pred.view(-1)
-    tflat = target.view(-1)
-    intersection = (iflat * tflat).sum()
-
-    return 1 - ((2. * intersection + smooth) /
-                (iflat.sum() + tflat.sum() + smooth))
+        p = (2. * intersection + self.smooth)
+        q = (iflat.sum() + tflat.sum() + self.smooth)
+        return (-1)*(p/q)
 
 
-class Loss(torch.nn.Module):
+class Loss(nn.Module):
     def __init__(self, pos_weight):
         super(Loss, self).__init__()
         self.bce_loss = torch.nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+        self.sigmoid = nn.Sigmoid()
+        self.dice_loss = DiceLoss()
 
     def forward(self, predicted, target):
         bce = self.bce_loss(predicted, target)
-        dice = dice_loss(predicted, target)
+        predicted_sm = self.sigmoid(predicted)
+        dice = self.dice_loss(predicted_sm, target)
 
         return bce + dice
